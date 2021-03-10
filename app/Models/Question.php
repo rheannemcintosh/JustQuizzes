080fbd61fd258e45ed303be8bdcb301f89cb47ec
rheannemcintosh <rheanne.mcintosh@abdn.ac.uk>
2021-03-10 23:46:00 +0000

    Update the Question model and migration

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -4,8 +4,29 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Answer;
+use App\Models\Quiz;
 
 class Question extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'question',
+        'quiz_id',
+    ];
+
+    public function answers(){
+        return $this->hasMany(Answer::class);
+    }
+
+    public function quiz(){
+        return $this->belongsTo(Quiz::class);
+    }
+
 }
