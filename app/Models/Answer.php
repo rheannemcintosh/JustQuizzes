559191278842149789cbd7c60878b57db2ed32c2
rheannemcintosh <rheanne.mcintosh@abdn.ac.uk>
2021-03-10 23:48:03 +0000

    Create the Answer model and migration

--- app/Models/Answer.php
+++ app/Models/Answer.php
@@ -4,8 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Question;
 
 class Answer extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'question_id',
+        'answer_id',
+        'is_correct',
+    ];
+
+    public function question(){
+        return $this->belongsTo(Question::class);
+    }
+
 }
