c5aa57dd8186b03bbd86c47961200281c41233d9
rheannemcintosh <rheanne.mcintosh@abdn.ac.uk>
2021-03-15 20:53:11 +0000

    Add a delete button and alert in the view to delete a quiz

--- resources/views/backend/quiz/index.blade.php
+++ resources/views/backend/quiz/index.blade.php
@@ -45,16 +45,25 @@
                                             </a>
                                         </td>
                                         <td>
-                                            <a href="">
-                                                <button class="btn btn-danger">
-                                                    Delete
-                                                </button>
-                                            </a>
+                                            <form id="delete-form{{ $quiz->id }}" method="POST" action="{{ route('quiz.destroy', [$quiz->id]) }}">@csrf
+                                                {{ method_field('DELETE') }}
+                                                <a 
+                                                    href="#"
+                                                    onclick="if(confirm('Do you want to delete the quiz?')){
+                                                        event.preventDefault();
+                                                        document.getElementById('delete-form{{ $quiz->id }}').submit();
+                                                    } else {
+                                                        event.preventDefault();
+                                                    }
+                                                ">
+                                                    <input type="submit" value="Delete" class="btn btn-danger">
+                                                </a>
+                                            </form>
                                         </td>
                                     <tr>
                                 @endforeach
                             @else
-                                <td colspan="4">No quizzes to display</td>
+                                <td colspan="6">No quizzes to display</td>
                             @endif
                         </tbody>
                     </table>
